4ffb214b9cb919f5ad3a11555d3b20c032848ce6
hzhreal <142254293+hzhreal@users.noreply.github.com>
2023-11-13 02:53:00 +0100

    Update main.py
    - idle kick
    - dotenv
    - improvements

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands, tasks
 import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_CHANNEL_ID = int(os.getenv("BOT_CHANNEL_ID"))
+QUEUE_CHANNEL_ID = int(os.getenv("QUEUE_CHANNEL_ID"))
+ROLE_NAME = str(os.getenv("ROLE_NAME"))
 
 activity = discord.Activity(type=discord.ActivityType.watching, name="HTO")
 
@@ -21,7 +28,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 embed_missing_role = discord.Embed(title="Missing role",
-                      description="Only the owner can use this command",
+                      description="Only the owner can use this command.",
                       color=0xFF0000)
 
 embed_missing_role.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133691278001979412/1145824413229522974/85204668-serious-bearded-male-security-guard-says-no-making-x-sign-shape.png")
@@ -32,11 +39,35 @@ queue_list = []
 update_role_running = True
 first_user_pinged = False
 
+@tasks.loop(seconds=1)
+async def idleListener(ctx, bot):
+    global first_user_pinged, queue_list
+
+    if len(queue_list) >= 1:
+        def check(message):
+            return message.author == queue_list[0]
+
+        try:
+            message = await bot.wait_for('message', check=lambda message:check(message), timeout=300) # how many seconds until idle kick
+            print(f"Obtained: {message.content}")
+        except asyncio.TimeoutError:
+            try:
+                target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id 
+                await target_channel.purge(limit=200)
+                print(f"purged {target_channel}")
+                role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME) # role name
+                removedUser = queue_list.pop(0)
+                await removedUser.remove_roles(role)
+                first_user_pinged = False
+                print(f"{removedUser} has idled, now removed\nCurrent queue:\n{queue_list}")
+            except Exception as e:
+                print(f"Error: {e}")
+
 async def changeperm(ctx):
     try:
-        role = discord.utils.get(ctx.guild.roles, name="HTOS-pass") # role name
-        target_channel = ctx.guild.get_channel(1145782703711604798) # bot channel id
-        target_channel2 = ctx.guild.get_channel(1146892703028760696) # queue channel id
+        role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME) # role name
+        target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id
+        target_channel2 = ctx.guild.get_channel(QUEUE_CHANNEL_ID) # queue channel id
         await target_channel2.set_permissions(ctx.guild.default_role, send_messages=False) # makes the channel you run the command in read only for @everyone
         await target_channel.set_permissions(ctx.guild.default_role, send_messages=False)
         await target_channel.set_permissions(role, send_messages=True)
@@ -45,30 +76,25 @@ async def changeperm(ctx):
         
     except Exception as e:
         # Handle any exceptions that occur during the permission modification
-        print(e)
-
-
+        print(f"Cant change permissions: {e}")
 
 @commands.has_role("Owner")
-@bot.slash_command(description="Start the queue")
+@bot.slash_command(description="Start the queue.")
 async def queue(ctx):
     global queue_list, update_role_running, first_user_pinged
 
     update_role_running = True
     first_user_pinged = False
-    target_channel2 = ctx.guild.get_channel(1146892703028760696) # queue channel id
+    target_channel2 = ctx.guild.get_channel(QUEUE_CHANNEL_ID) # queue channel id
 
     await ctx.respond("Starting", ephemeral=True)
 
     await changeperm(ctx)
 
-
-
     class queuebutton(discord.ui.View):
 
         def __init__(self):
             super().__init__(timeout=None) # specify the timeout here
-
 
         @discord.ui.button(label="Join queue", style=discord.ButtonStyle.success)
         async def callback(self, button, interaction):
@@ -79,13 +105,13 @@ async def queue(ctx):
             if author in queue_list:
                 if author == queue_list[0]:
                     try:
-                        target_channel = ctx.guild.get_channel(1145782703711604798) # bot channel id 
+                        target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id 
                         await target_channel.purge(limit=200)
                         print(f"purged {target_channel}")
                     except Exception as e:
                         print(f"Error purging: {e}")
                     queue_list.remove(author)  # Remove the user from the queue
-                    role = discord.utils.get(ctx.guild.roles, name="HTOS-pass") # role name
+                    role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME) # role name
                     await author.remove_roles(role)
                     await interaction.response.send_message("Left queue", ephemeral=True)
                     first_user_pinged = False
@@ -131,12 +157,10 @@ async def queue(ctx):
             # Assuming "HTOS-pass" is a role object, you should add the role, not just a string
             role = discord.utils.get(ctx.guild.roles, name="HTOS-pass") # role name
             await first_in.add_roles(role)
-
-        if len(queue_list) > 0:
             if not first_user_pinged:
-                target_channel = ctx.guild.get_channel(1145782703711604798) # bot channel id
+                target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id
                 await asyncio.sleep(2.5)
-                await target_channel.send(f"It is your turn {first_in.mention}, please click the join queue button again when you are finished to leave the queue")
+                await target_channel.send(f"It is your turn {first_in.mention}, please click the join queue button again when you are finished to leave the queue.")
                 first_user_pinged = True
                 
         if not update_role_running:
@@ -144,9 +168,8 @@ async def queue(ctx):
             print("stopped loop")  # Stop the loop
             return
 
-        
-
-    await update_queue_embed.start()
+    update_queue_embed.start()
+    idleListener.start(ctx, bot)
 
 @queue.error
 async def role_error(ctx, error):
@@ -154,27 +177,27 @@ async def role_error(ctx, error):
         await ctx.respond(embed=embed_missing_role)
 
 @commands.has_role("Owner")
-@bot.slash_command(description="End the queue")
+@bot.slash_command(description="End the queue.")
 async def end(ctx):
     global update_role_running, queue_list
 
-    await ctx.respond("Shutting down queue", ephemeral=True)
+    await ctx.respond("Shutting down queue.", ephemeral=True)
 
     update_role_running = False
     queue_list = []
 
-    role = discord.utils.get(ctx.guild.roles, name="HTOS-pass") # role name
+    role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME) # role name
 
     await role.delete()
     print("deleted role")
 
    
-    await ctx.guild.create_role(name="HTOS-pass")
+    await ctx.guild.create_role(name=ROLE_NAME) # role name
     print("created role")
 
     try:
-        target_channel = ctx.guild.get_channel(1145782703711604798) # bot channel id
-        target_channel2 = ctx.guild.get_channel(1146892703028760696) # queue channel id
+        target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id
+        target_channel2 = ctx.guild.get_channel(QUEUE_CHANNEL_ID) # queue channel id
         await target_channel.purge(limit=200)
         print(f"purged {target_channel}")
         await target_channel2.purge(limit=10)
@@ -182,25 +205,25 @@ async def end(ctx):
     except Exception as e:
         print(f"Error purging: {e}")
 
-    await ctx.respond("Shut down queue", ephemeral=True)
+    await ctx.respond("Shut down queue.", ephemeral=True)
             
 @end.error
 async def role_error1(ctx, error):
     if isinstance(error, commands.MissingRole):
         await ctx.respond(embed=embed_missing_role)
 
-@bot.slash_command(description="Removes an user from the queue")
+@bot.slash_command(description="Removes an user from the queue.")
 @commands.has_any_role("Owner", "Admin", "Mod")
 async def remove(ctx, user: discord.Member):
     global first_user_pinged, queue_list
 
-    embedrm = discord.Embed(title="Remove user", description=f"Removed {user} from list", color=discord.Color.green())
+    embedrm = discord.Embed(title="Remove user", description=f"Removed {user} from list.", color=discord.Color.green())
     embedrm.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133691278001979412/1145825848801370203/3d-security-agent-pointing-to-empty-wall.png")
             
     embedrm.set_footer(text="Made by: hzh.")
 
     if len(queue_list) == 0:
-        embederror1 = discord.Embed(title="Error: Empty queue", description="The queue is empty", color=discord.Color.red())
+        embederror1 = discord.Embed(title="Error: Empty queue", description="The queue is empty.", color=discord.Color.red())
         embederror1.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133691278001979412/1145825848801370203/3d-security-agent-pointing-to-empty-wall.png")
             
         embederror1.set_footer(text="Made by: hzh.")
@@ -209,12 +232,12 @@ async def remove(ctx, user: discord.Member):
        
     elif user == queue_list[0]:
             try:
-                target_channel = ctx.guild.get_channel(1145782703711604798) # bot channel id 
+                target_channel = ctx.guild.get_channel(BOT_CHANNEL_ID) # bot channel id 
                 await target_channel.purge(limit=200)
                 print(f"purged {target_channel}")
             except Exception as e:
                 print(f"Error purging: {e}")
-            role = discord.utils.get(ctx.guild.roles, name="HTOS-pass") # role name
+            role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME) # role name
             queue_list.remove(user)
             await user.remove_roles(role)
             first_user_pinged = False
@@ -226,7 +249,7 @@ async def remove(ctx, user: discord.Member):
         await ctx.respond(embed=embedrm)
 
     else:
-        embederror = discord.Embed(title="Error: Invalid user", description=f"{user} is not present in the queue", color=discord.Color.red())
+        embederror = discord.Embed(title="Error: Invalid user", description=f"{user} is not present in the queue.", color=discord.Color.red())
         embederror.set_thumbnail(url="https://cdn.discordapp.com/attachments/1133691278001979412/1145825848801370203/3d-security-agent-pointing-to-empty-wall.png")
             
         embederror.set_footer(text="Made by: hzh.")
@@ -238,9 +261,9 @@ async def role_error2(ctx, error):
     if isinstance(error, commands.MissingAnyRole):
         await ctx.respond(embed=embed_missing_role)
 
-@bot.slash_command(description="Pings the bot")
+@bot.slash_command(description="Pings the bot.")
 async def ping(ctx):
     latency = bot.latency * 1000
-    await ctx.respond(f"Pong! {latency: .2f}ms")          
+    await ctx.respond(f"Pong! {latency: .2f}ms.")       
 
-bot.run("") # token
+bot.run(str(os.getenv("TOKEN"))) # token
